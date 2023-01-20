8e72b32b0e39548ab71eea9384ad3b5ecdf61881
Gabriel Germano <ggermanow279@gmail.com>
2023-01-20 16:21:23 -0300

    exercicio4

---
+++ estruturas-repeticao/src/exercicioArea.java
@@ -0,0 +1,26 @@
+
+
+public class exercicioArea {
+
+    public static void main(String[]args){ 
+        exercicioArea.calcularArea(3);
+        }
+
+    public static void calcularArea(double lado1, double lado2){
+	    double area = lado1 * lado2;
+		System.out.print("O valor da área é: "+area);
+
+    }
+
+    public static void calcularArea(double lado){
+        double area = lado * lado;
+        System.out.print("O valor da área é: "+area);
+    }
+
+    public static void calcularArea(double lado1, double lado2, double lado3){
+        double area = ((lado1+lado2)*lado3)*1/2;
+        System.out.print("O valor da área é: "+area);
+
+    }
+
+}
